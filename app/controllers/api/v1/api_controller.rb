13f6d7688538f96f3ff103d0c384247e968bcc2f
Brian Hogg <brian@hoggworks.com>
2018-02-03 16:04:58 -0500

    Updated security check to not care about case-sensitivity in header names

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -12,14 +12,33 @@ module Api::V1
     end
 
     def get_current_user_2
-      if request.headers['Access-Token'].nil? or request.headers['Client'].nil? or request.headers['Uid'].nil?
+      if !request.headers['Access-Token'].nil?
+        token = request.headers['Access-Token']
+      elsif !request.headers['access-token'].nil?
+        token = request.headers['access-token']
+      end
+
+      if !request.headers['Client'].nil?
+        client = request.headers['Client']
+      elsif !request.headers['client'].nil?
+        client = request.headers['client']
+      end
+
+      if !request.headers['Uid'].nil?
+        uid = request.headers['Uid']
+      elsif !request.headers['uid'].nil?
+        uid = request.headers['uid']
+      end
+
+
+      if !uid or !client or !token
         return nil
       end
-      current_user = User.find_by(uid: request.headers['Uid'])
 
+      current_user = User.find_by(uid: uid)
 
-      if current_user && current_user.tokens.has_key?(request.headers["Client"])
-          token = current_user.tokens[request.headers["Client"]]
+      if current_user && current_user.tokens.has_key?(client)
+          token = current_user.tokens[client]
           expiration_datetime = DateTime.strptime(token["expiry"].to_s, "%s")
 
           if expiration_datetime > DateTime.now

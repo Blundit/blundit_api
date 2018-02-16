bda10a2b062cbfaaf51f60ab9135205427562cf7
Brian Hogg <brian@hoggworks.com>
2018-02-16 12:40:53 -0500

    Tweaks to hopefully get token validation working again

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Blundit
         origins '*'
         resource '*',
           :headers => :any, 
-          :expose => ['Access-Token', 'Expiry', 'Token-Type', 'Uid', 'Client'],
+          :expose => ['Access-Token', 'access-token', 'expiry', 'token-type', 'uid', 'client', 'Expiry', 'Token-Type', 'Uid', 'Client'],
           :methods => [:get, :post, :patch, :delete, :options, :put]
       end
     end

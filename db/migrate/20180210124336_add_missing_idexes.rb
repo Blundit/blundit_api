88229266819940997c960e5e193409d54d1ab108
Brian Hogg <brian@hoggworks.com>
2018-02-23 18:06:45 -0500

    Updated migrate script

--- db/migrate/20180210124336_add_missing_idexes.rb
+++ db/migrate/20180210124336_add_missing_idexes.rb
@@ -1,46 +1,43 @@
 class AddMissingIdexes < ActiveRecord::Migration
   def change
-    add_index :bona_fides, :expert_id
-    add_index :bookmarks, :claim_id
-    add_index :bookmarks, :expert_id
-    add_index :bookmarks, :prediction_id
-    add_index :bookmarks, :user_id
-    add_index :claim_categories, :category_id
-    add_index :claim_categories, :claim_id
-    add_index :claim_comments, :claim_id
-    add_index :claim_comments, :comment_id
-    add_index :claim_evidences, :claim_id
-    add_index :claim_evidences, :evidence_id
-    add_index :claim_experts, :claim_id
-    add_index :claim_experts, :expert_id
-    add_index :claim_flags, :claim_id
-    add_index :claim_flags, :flag_id
-    add_index :claim_votes, :claim_id
-    add_index :claim_votes, :vote_id
-    add_index :comments, :user_id
-    add_index :contributions, :bona_fide_id
-    add_index :contributions, :claim_id
-    add_index :contributions, :comment_id
-    add_index :contributions, :evidence_id
-    add_index :contributions, :expert_id
-    add_index :contributions, :flag_id
-    add_index :contributions, :prediction_id
-    add_index :contributions, :user_id
-    add_index :expert_categories, :category_id
-    add_index :expert_categories, :expert_id
-    add_index :expert_category_accuracies, :category_id
-    add_index :expert_category_accuracies, :expert_id
-    add_index :expert_claims, :claim_id
-    add_index :expert_claims, :expert_id
-    add_index :expert_comments, :comment_id
-    add_index :expert_comments, :expert_id
-    add_index :expert_flags, :expert_id
-    add_index :expert_flags, :flag_id
-    add_index :expert_predictions, :expert_id
-    add_index :expert_predictions, :prediction_id
-    add_index :flags, :claim_flag_id
-    add_index :flags, :expert_flag_id
-    add_index :flags, :prediction_flag_id
+    # add_index :bona_fides, :expert_id
+    # add_index :bookmarks, :claim_id
+    # add_index :bookmarks, :expert_id
+    # add_index :bookmarks, :prediction_id
+    # add_index :bookmarks, :user_id
+    # add_index :claim_categories, :category_id
+    # add_index :claim_categories, :claim_id
+    # add_index :claim_comments, :claim_id
+    # add_index :claim_comments, :comment_id
+    # add_index :claim_evidences, :claim_id
+    # add_index :claim_evidences, :evidence_id
+    # add_index :claim_experts, :claim_id
+    # add_index :claim_experts, :expert_id
+    # add_index :claim_flags, :claim_id
+    # add_index :claim_flags, :flag_id
+    # add_index :claim_votes, :claim_id
+    # add_index :claim_votes, :vote_id
+    # add_index :comments, :user_id
+    # add_index :contributions, :bona_fide_id
+    # add_index :contributions, :claim_id
+    # add_index :contributions, :comment_id
+    # add_index :contributions, :evidence_id
+    # add_index :contributions, :expert_id
+    # add_index :contributions, :flag_id
+    # add_index :contributions, :prediction_id
+    # add_index :contributions, :user_id
+    # add_index :expert_categories, :category_id
+    # add_index :expert_categories, :expert_id
+    # add_index :expert_category_accuracies, :category_id
+    # add_index :expert_category_accuracies, :expert_id
+    # add_index :expert_claims, :claim_id
+    # add_index :expert_claims, :expert_id
+    # add_index :expert_comments, :comment_id
+    # add_index :expert_comments, :expert_id
+    # add_index :expert_flags, :expert_id
+    # add_index :expert_flags, :flag_id
+    # add_index :expert_predictions, :expert_id
+    # add_index :expert_predictions, :prediction_id
     add_index :notification_queue_items, :category_id
     add_index :notification_queue_items, :claim_id
     add_index :notification_queue_items, :comment_id

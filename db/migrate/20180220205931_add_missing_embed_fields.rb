966dddb0d19e8e36cbee728eee8951d86804d7db
Brian Hogg <brian@hoggworks.com>
2018-02-23 18:10:01 -0500

    Again with some migrate errors

--- db/migrate/20180220205931_add_missing_embed_fields.rb
+++ db/migrate/20180220205931_add_missing_embed_fields.rb
@@ -1,8 +1,8 @@
 class AddMissingEmbedFields < ActiveRecord::Migration[5.0]
   def change
-    add_column :embeds, :user_id, :integer, length: 11
-    add_column :embeds, :title, :string
-    add_column :embeds, :description, :text
-    add_column :embeds, :show_chrome, :boolean, default: true
+    # add_column :embeds, :user_id, :integer, length: 11
+    # add_column :embeds, :title, :string
+    # add_column :embeds, :description, :text
+    # add_column :embeds, :show_chrome, :boolean, default: true
   end
 end
